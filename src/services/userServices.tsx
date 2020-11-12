490796300f1023e73d567ee1d2e2aaca668230ad
Joe van der Zwet <me@joezwet.me>
2020-11-12 13:50:21 +1300

    feat: add method for getting user by email

--- src/services/userServices.tsx
+++ src/services/userServices.tsx
@@ -1,34 +1,36 @@
 import http from "./HttpCommon";
 
-class UserDataService{
-    getAll() {
-        return http.get("/users");
-      }
-    
-      get(id: number) {
-        return http.get(`/users/${id}`);
-      }
-      getByName(name: string) {
-        return http.get(`/users/${name}`);
-      }
-      create(data: any) {
-        return http.post("/user", data);
-      }
-    
-      update(id: number, data: any) {
-        return http.put(`/user/${id}`, data);
-      }
-    
-      delete(id: number) {
-        return http.delete(`/user/${id}`);
-      }
-    
-    //   deleteAll() {
-    //     return http.delete(`/tutorials`);
-    //   }
-    
-      login(username: string, password: string) {
-        return http.get(`/login/${username}/${password}`);
-      }
+class UserDataService {
+  getAll() {
+    return http.get("/users");
+  }
+  get(id: number) {
+    return http.get(`/users/${id}`);
+  }
+  getByName(name: string) {
+    return http.get(`/users/${name}`);
+  }
+  getByEmail(email: string) {
+    return http.get(`/users/${email}`);
+  }
+  create(data: any) {
+    return http.post("/user", data);
+  }
+
+  update(id: number, data: any) {
+    return http.put(`/user/${id}`, data);
+  }
+
+  delete(id: number) {
+    return http.delete(`/user/${id}`);
+  }
+
+  //   deleteAll() {
+  //     return http.delete(`/tutorials`);
+  //   }
+
+  login(username: string, password: string) {
+    return http.get(`/login/${username}/${password}`);
+  }
 }
 export default new UserDataService();
