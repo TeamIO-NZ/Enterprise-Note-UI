52542ca6efb9a485ff2e352d6bf092b2b340d9f3
lithial <Jameshessell@gmail.com>
2020-11-14 11:45:27 +1300

    feat: possible working avatar buttons

--- src/elements/NoteShareGroup.tsx
+++ src/elements/NoteShareGroup.tsx
@@ -6,45 +6,66 @@ import { Note } from '../models/Note';
 import User from '../models/User';
 import UserServices from '../services/UserServices';
 
-export default function NoteShareGroup({note}: {note: Note}) {
+export default function NoteShareGroup({ note }: { note: Note }) {
 
   const [owner, setOwner] = React.useState<Array<User>>([]);
   const [editors, setEditors] = React.useState<Array<User>>([]);
   const [viewers, setViewers] = React.useState<Array<User>>([]);
 
   const genGroup = (users: Array<User>): any => {
-
-    return (
-      <AvatarGroup max={4}>
-        <Avatar>FUCK</Avatar>
-        {
-          users.map((u: User) => {
-            <Tooltip title={u.name}>
-              <Avatar>{u.name.charAt(0).toUpperCase()}</Avatar>
-            </Tooltip>
-          })
+    if (users.length > 0) {
+      console.log(`user:${users[0].name}`)
+      return (
+        <AvatarGroup max={4}>
+          {
+            users.map((u: User) => React.cloneElement(
+              <Tooltip title={u.name}>
+                <Avatar alt={u.name}>{u.name.charAt(0).toUpperCase().toString()}</Avatar>
+              </Tooltip>, {
+              key: u.id
+            }
+            ))
         }
-      </AvatarGroup>
-    );
+        </AvatarGroup>
+      );
+    }
+    else {
+      return null;
+    }
   }
 
   useEffect(() => {
-    console.log(note);
-    setOwner([]);
-    setEditors([]);
-    setViewers([]);
+    // setOwner([]);
+    // setEditors([]);
+    // setViewers([]);
 
+    // UserServices.get(note.owner)
+    //   .then(res => {
+    //     console.log(res);
+    //     if (res.data) {
+    //       var userInfo = res.data;
+
+    //       owner.push(new User(userInfo.name, "", false, "", "", "", userInfo.userId));
+    //       //setOwner(o);
+    //       console.log(`owner is ${owner[0]}`);
+    //     }
+    //   });
     UserServices.get(note.owner)
-      .then(res => {
-        console.log(res);
-        if (res.data) {
-          var userInfo = res.data;
-          let o = owner;
-          o.push(new User(userInfo.name, "", false, "", "", "", userInfo.userId));
-          setOwner(o);
-          console.log(o);
+      .then(data => {
+        console.log(data)
+        var n: Array<User> = [];
+        if (data === undefined || data === null) return [];
+        //console.log(data.data) 
+        var u = data.data;
+        n.push(new User(u.name, "", false, "", "", "", u.userId));
+
+        return n;
+      })
+      .then(
+        (users: Array<User>) => {
+          setOwner(users);
         }
-      });
+      )
 
     for (const id of note.editors) {
       UserServices.get(id)
@@ -76,7 +97,6 @@ export default function NoteShareGroup({note}: {note: Note}) {
 
   return (
     <div>
-      <Typography variant="body2">pog</Typography>
       <Grid container spacing={3}>
         <Grid item xs={4}>
           {
@@ -88,7 +108,7 @@ export default function NoteShareGroup({note}: {note: Note}) {
             genGroup(editors)
           }
         </Grid>
-        
+
         <Grid item xs={4}>
           {
             genGroup(viewers)
